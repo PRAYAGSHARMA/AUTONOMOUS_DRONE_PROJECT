3b946fbe6e77c8185891a2213afc3acf0bfea266
Vinit Jha <jhavinit07@gmail.com>
2019-10-03 17:01:57 +0530

    Updated time sleep data

--- connection.py
+++ connection.py
@@ -25,16 +25,11 @@ def connectMyCopter():
     parser = argparse.ArgumentParser(description='commands')
     parser.add_argument('--connect')
     args = parser.parse_args()
-
     connection_string = args.connect
-
     vehicle = connect(connection_string,wait_ready=True)
-
     return vehicle
- 
- 
 
 vehicle = connectMyCopter()
-vehicle.mode    = VehicleMode("LAND") 
-time.sleep(15)
+vehicle.mode = VehicleMode("LAND") 
+time.sleep(15)                      #Sleep time required for copter to land
 vehicle.close()      
